1f333ceb32e9accb12b23c5fd7709eed2c939ea4
Jabes Voltatoni <voltatoni.si@gmail.com>
2024-08-26 16:25:12 -0300

    Em andamento

---
+++ Aula 11/ex012.js
@@ -0,0 +1,11 @@
+var vel = 50
+var vmax = 50
+
+console.log(`A velocidade do carro é ${vel}km`)
+
+if(vel > vmax){
+    console.log(`Você ultrapassou a velocidade a velocidade permitida de ${vmax}km/h. MULTADO!`)
+}
+    console.log(`Dirija sempre utilizando cinto de segurança`)
+
+
---
+++ Aula 11/ex013.js
@@ -0,0 +1,7 @@
+var país = 'EUA'
+console.log(`País de origem ${país}`)
+if(país == 'BRL'){
+    console.log('Você é Brasíleiro')
+}else{
+    console.log('Você é Estrangeiro')
+}
