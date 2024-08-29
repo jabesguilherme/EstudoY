3af3c5996ddfe4d309cdbccc177e5fda7b48a9cf
Jabes Voltatoni <voltatoni.si@gmail.com>
2024-08-29 16:19:07 -0300

    Exercício contagem de passos

---
+++ Exe/Exe003/script.js
@@ -0,0 +1,33 @@
+function contar(){
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        res.innerHTML = '[ERRO] Impossível contar, faltam dados!'
+    }else{
+        res.innerHTML = 'BOOM: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        if(p <= 0){
+            alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+
+        if(i < f){
+            // Contagem crescente
+            for(let c = i; c <= f; c += p){
+                res.innerHTML += `${c} \u{1F496}`
+            }
+        }else {
+            //Contagem regressiva
+            for(let c = i; c >= f; c -= p){
+                res.innerHTML += `${c} \u{1F496}`
+            } 
+        }
+        res.innerHTML += `\u{1F3C1} Chegou no Amor`
+    }
+}
