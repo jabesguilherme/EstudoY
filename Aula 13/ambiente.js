37ba2637a29c85d734aeafea0de7e4f1744f4832
Jabes Voltatoni <voltatoni.si@gmail.com>
2024-08-29 15:07:56 -0300

    Estrutura While, doWhile e for

---
+++ Aula 13/ambiente.js
@@ -0,0 +1,46 @@
+ 
+
+for(var c=1; c <= 10; c++){
+    console.log("Muito bem")
+ }
+
+
+
+
+                        /* UTILIZANDO for 
+ 
+ 
+ for(var c=1; c <= 10; c++){
+    console.log("Muito bem")
+ }
+ 
+ */
+                        /*  UTILIZANDO do while - ESTRUTURA BOA! 
+var c = 1
+
+do{
+    console.log(`Passo ${c}`)
+    c++
+}while(c <= 6)
+
+*/
+
+/* 
+                         UTILIZANDO WHILE - MELHOR ESTRUTURA!
+var c = 1
+
+while(c <= 6){
+    console.log(`Passo ${c}`)
+    c++
+}
+
+*/
+
+/*
+                        UTILIZANDO CONSOLE.LOG - PODRE DE RUIM!
+console.log(`Passo 1`)
+console.log(`Passo 2`)
+console.log(`Passo 3`)
+console.log(`Passo 4`)
+
+*/
